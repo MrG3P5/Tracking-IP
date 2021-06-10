f8ed916653158f7f5eb0c480aa574dce648e2ac6
MrG3P5 <xmrg3p5@gmail.com>
2021-06-10 21:23:32 +0700

    Up

---
+++ README.md
@@ -0,0 +1,23 @@
+### Install On TERMUX
+```bash
+> apt update && apt upgrade -y
+> pkg install git -y
+> pkg install nodejs -y
+> git clone https://github.com/MrG3P5/Brainly-Scraper.git
+> cd Brainly-Scraper
+> npm install
+> npm start
+```
+
+### Install On Windows
+
+- Download NodeJS latest in <a href="https://nodejs.org/en/download/current"><font>Here</a>
+- Download Git bash latest at <a href="https://git-scm.com/downloads"><font>Here</a>
+- If done open terminal
+
+```bash
+> git clone https://github.com/MrG3P5/Brainly-Scraper.git
+> cd Brainly-Scraper
+> npm install
+> npm start
+```
---
+++ geps.js
@@ -0,0 +1,72 @@
+/*
+* Hanya gabut:v
+* Don't be a script kidde:)
+*/
+
+// MODULE
+const readline = require('readline');
+const chalk = require('chalk');
+const ip2location = require('ip-to-location');
+
+// INPUT OUTPUT
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// COLOR
+const color = (text, color) => {
+    return !color ? chalk.green(text) : chalk.keyword(color)(text)
+}
+// MY FUNCTION
+function track(ip) {
+    ip2location.fetch(ip, function (err, res) {
+        if (err) {
+            return err;
+        } else {
+            return res;
+        }
+    })
+}
+
+
+// BANNER
+console.clear()
+console.log(color("..............", 'red'))
+console.log(color("            ..,;:ccc,.", 'red'))
+console.log(color("          ......''';lxO.", 'red'))
+console.log(color(".....''''..........,:ld;", 'red'))
+console.log(color("           .';;;:::;,,.x,", 'red'))
+console.log(color("      ..'''.            0Xxoc:,.  ...", 'red'))
+console.log(color("  ....                ,ONkc;,;cokOdc',.", 'red'))
+console.log(color(" .                   OMo           ':ddo.", 'red'))
+console.log(color("                    dMc               :OO;", 'red'))
+console.log(color("                    0M.                 .:o.", 'red'))
+console.log(color("                    ;Wd", 'red'))
+console.log(color("                     ;XO,", 'red'))
+console.log(color("                       ,d0Odlc;,..", 'red'))
+console.log(color("                           ..',;:cdOOd::,.", 'red'))
+console.log(color("                                    .:d;.':;.", 'red'))
+console.log(color("                                       'd,  .'", 'red'))
+console.log(color("                                         ;l   ..", 'red'))
+console.log(color("                                          .o", 'red'))
+console.log(color("    [=] Made by : X - MrG3P5 [=]            ", 'cyan'), color("c", "red"))
+console.log(color("    [=] Version : 2.0        [=]             ", 'cyan'), color(".'", "red"))
+console.log(color("                                              ", 'cyan'), color(".'", "red"))
+console.log('')
+console.log('')
+
+// USER INPUT QUESTION
+var recursiveAsyncReadLine = function () {
+    rl.question('\033[1;36mIP: \033[1;32m', function (userInput) {
+        if (userInput == 'exit' || userInput == 'Exit' || userInput == 'EXIT' || userInput == '') {
+            console.log('See you...^.^')
+            process.exit();
+        }
+        ip2location.fetch(userInput, function (err, res) {
+            console.log(res)
+                recursiveAsyncReadLine();
+        })
+    })
+} // CALLBACK INPUT
+recursiveAsyncReadLine();
